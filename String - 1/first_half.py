542cb472147cd502441c1b72b725684070ca3940
Peretz Cohen <pizzaz93@users.noreply.github.com>
2020-10-07 09:01:40 -0700

    Add files via upload

---
+++ String - 1/first_half.py
@@ -0,0 +1,12 @@
+"""
+Given a string of even length, return the first half.
+So the string "WooHoo" yields "Woo".
+
+
+first_half('WooHoo') → 'Woo'
+first_half('HelloThere') → 'Hello'
+first_half('abcdef') → 'abc'
+"""
+
+def first_half(str):
+  return str[0:len(str) / 2]
---
+++ String - 1/without_end.py
@@ -0,0 +1,16 @@
+"""
+
+Given a string, return a version without the first and last char,
+so "Hello" yields "ell". The string length will be at least 2.
+
+
+without_end('Hello') → 'ell'
+without_end('java') → 'av'
+without_end('coding') → 'odin'
+
+"""
+
+def without_end(str):
+  if len(str) == 2:
+    return ""
+  return str[1:len(str) - 1]
