bc155c7f9087bf20d94f21a102fa193ed12ff649
Peretz Cohen <pizzaz93@users.noreply.github.com>
2020-10-12 10:06:38 -0700

    Create rotate_left3.py

---
+++ List - 1/rotate_left3.py
@@ -0,0 +1,17 @@
+"""
+
+Given an array of ints length 3, return an array
+with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7]
+
+"""
+
+def rotate_left3(nums):
+  newList = nums
+  x = newList.pop(0)
+  newList.append(x)
+  return newList
